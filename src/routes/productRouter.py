6c16fc5cb518813875544db8de066569b36e2b75
Yuliia <you@example.com>
2024-03-26 17:01:20 +0100

    metodos Post, Patch, Delete

--- src/routes/productRouter.py
+++ src/routes/productRouter.py
@@ -1,10 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint,  request
 from src.services.productService import ProductService
+from src.models.productModel import Product
 
 main = Blueprint('mgmalagenos_blueprint', __name__)
 
-@main.route('/')
+@main.route('/', methods = [ 'GET','POST','PATCH','DELETE'])
 def manage_product():
-    ProductService.get_product()
+    
+    if request.method == "GET":
+        get_product = ProductService.get_product()
+        print(get_product)
+    
+    elif request.method == "POST":
+        nombre_producto = request.json['nombre_producto']
+        descr_producto = request.json['descr_producto']
+        marca_producto = request.json['marca_producto']
+        precio_producto = request.json['precio_producto']
+        stock_producto = request.json['stock_producto']   
+        
+        product_table = Product(0,nombre_producto,
+                                descr_producto, 
+                                marca_producto,
+                                precio_producto,
+                                stock_producto)
+        post_product = ProductService.post_product(product_table)
+        print(post_product)
+        
+        
+    elif request.method == "PATCH": 
+        id_producto = request.json['id_producto']
+        updates = request.json['updates']
+        patch_product = ProductService.patch_product(id_producto, updates)
+        print(patch_product) 
+    
+    elif request.method == "DELETE":
+        id_producto = request.json['id_producto']
+        delete_product = ProductService.delete_product(id_producto)
+        print(delete_product)  
+           
     print('Esto se imprime en consola')
     return 'Esto se ve en la pagina'
--- src/services/productService.py
+++ src/services/productService.py
@@ -1,5 +1,5 @@
 from src.database.db_phpMySql import get_connection
-
+from src.models.productModel import Product
 class ProductService():
     
     @classmethod
@@ -15,5 +15,67 @@ class ProductService():
             connection.close()
             return "Data base is close"
         
+        except Exception as ex:
+            print(ex)
+    
+    @classmethod
+    def post_product(cls, product_table:Product):
+        try:
+            connection  = get_connection()
+            #print(connection)
+            id_producto = product_table.id_producto
+            nombre_producto = product_table.nombre_producto
+            descr_producto = product_table.descr_producto 
+            marca_producto = product_table.marca_producto
+            precio_producto = product_table.precio_producto
+            stock_producto = product_table.stock_producto
+            
+            
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO producto (id_producto, nombre_producto,descr_producto, marca_producto, precio_producto, stock_producto) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')"
+                               .format(id_producto, nombre_producto, descr_producto, marca_producto, precio_producto, stock_producto))
+                connection.commit()
+                #print(cursor)
+                
+            connection.close()
+            return "Data base is close"
+        
+        except Exception as ex:
+            print(ex)    
+            
+                
+    @classmethod
+    def patch_product(cls, id_producto, updates):
+        try:
+            connection = get_connection()
+            
+            with connection.cursor() as cursor:
+                # Створюємо рядок для SQL-запиту для оновлення курсу
+                set_clause = ", ".join([f"{key} = '{value}'" for key, value in updates.items()])
+                
+                # Виконуємо SQL-запит PATCH для оновлення курсу з вказаним ID
+                cursor.execute(f'UPDATE producto SET {set_clause} WHERE producto.id_producto = %s', (id_producto,))
+                
+                connection.commit()
+                
+            connection.close()
+            return "Data base is close"
+            
+        except Exception as ex:
+            print(ex)
+            
+    @classmethod
+    def delete_product(cls, id_producto): 
+        try:
+            connection  = get_connection()
+            print(connection)
+            
+            with connection.cursor() as cursor:
+                cursor.execute('DELETE FROM producto WHERE producto.id_producto = %s', (id_producto))
+                connection.commit()
+                
+            connection.close()
+            return "Data base is close"
+            
         except Exception as ex:
             print(ex)
